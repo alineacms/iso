a35e02d30fe29f6d3df1c2b1fc3a0642d8dafe35
Ben Merckx <ben@codeurs.be>
2022-12-05 11:35:42 +0100

    Be more explicit about what we're exporting. This works around a next/webpack bug where our exports are not correctly picked up: (Attempted import error: 'fetch' is not exported from '@alinea/iso')

--- src/index.node.js
+++ src/index.node.js
@@ -1,4 +1,29 @@
-export * from './crypto.node.js'
-export * from './streams.node.js'
-export * from './fetch.node.js'
-export * from './blob.node.js'
+export {crypto} from './crypto.node.js'
+export {
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  ReadableStream,
+  ReadableByteStreamController,
+  ReadableStreamBYOBReader,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+  TransformStream,
+  TransformStreamDefaultController,
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+  CompressionStream,
+  DecompressionStream
+} from './streams.node.js'
+export {
+  AbortController,
+  AbortSignal,
+  File,
+  FormData,
+  Headers,
+  Request,
+  Response,
+  fetch
+} from './fetch.node.js'
+export {atob, btoa, Blob} from './blob.node.js'
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-export * from './crypto.js'
-export * from './streams.js'
-export * from './fetch.js'
-export * from './blob.js'
+export {crypto} from './crypto.js'
+export {
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  ReadableStream,
+  ReadableByteStreamController,
+  ReadableStreamBYOBReader,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+  TransformStream,
+  TransformStreamDefaultController,
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+  CompressionStream,
+  DecompressionStream
+} from './streams.js'
+export {
+  AbortController,
+  AbortSignal,
+  File,
+  FormData,
+  Headers,
+  Request,
+  Response,
+  fetch
+} from './fetch.js'
+export {atob, btoa, Blob} from './blob.js'
