e38b3df3d4beb80c95c53d0405f43e76ed297c8a
Ben Merckx <ben@codeurs.be>
2024-09-16 14:23:07 +0200

    Use bun-compression streams implementation

--- src/streams.bun.js
+++ src/streams.bun.js
@@ -14,71 +14,56 @@ export const {
   WritableStreamDefaultWriter
 } = globalThis
 
-import {createDeflate, createGunzip, createGzip, createInflate} from 'node:zlib'
+/**
+ * (c) 2023 Kitpipat Jaritwong
+ * https://github.com/sunneydev/bun-compression
+ */
 
-function transform(transformer) {
-  return new TransformStream({
-    start(controller) {
-      transformer.on('data', data => {
-        controller.enqueue(data)
-      })
-    },
-    transform(buffer) {
-      transformer.write(buffer)
-    },
-    flush() {
-      return new Promise(resolve => {
-        transformer.flush(() => resolve())
-      })
-    }
-  })
-}
-
-const kTransform = Symbol('transform')
+import zlib from 'node:zlib'
 
 export class CompressionStream {
   constructor(format) {
-    switch (format) {
-      case 'deflate':
-        this[kTransform] = transform(createDeflate())
-        break
-      case 'gzip':
-        this[kTransform] = transform(createGzip())
-        break
-      default:
-        throw new TypeError('format must be one of `deflate`, `gzip`')
-    }
-  }
+    const handle =
+      format === 'deflate'
+        ? zlib.createDeflate()
+        : format === 'gzip'
+        ? zlib.createGzip()
+        : zlib.createDeflateRaw()
 
-  get readable() {
-    return this[kTransform].readable
-  }
+    this.readable = new ReadableStream({
+      start(controller) {
+        handle.on('data', chunk => controller.enqueue(chunk))
+        handle.once('end', () => controller.close())
+      }
+    })
 
-  get writable() {
-    return this[kTransform].writable
+    this.writable = new WritableStream({
+      write: chunk => handle.write(chunk),
+      close: () => handle.end()
+    })
   }
 }
 
 export class DecompressionStream {
   constructor(format) {
-    switch (format) {
-      case 'deflate':
-        this[kTransform] = transform(createInflate())
-        break
-      case 'gzip':
-        this[kTransform] = transform(createGunzip())
-        break
-      default:
-        throw new TypeError('format must be one of `deflate`, `gzip`')
-    }
-  }
+    const handle =
+      format === 'deflate'
+        ? zlib.createInflate()
+        : format === 'gzip'
+        ? zlib.createGunzip()
+        : zlib.createInflateRaw()
 
-  get readable() {
-    return this[kTransform].readable
-  }
+    this.readable = new ReadableStream({
+      start(controller) {
+        handle.on('data', chunk => controller.enqueue(chunk))
+        handle.once('end', () => controller.close())
+      }
+    })
 
-  get writable() {
-    return this[kTransform].writable
+    this.writable = new WritableStream({
+      write: chunk => handle.write(chunk),
+      close: () => handle.end()
+    })
   }
 }
 
@@ -89,60 +74,62 @@ export class DecompressionStream {
  * https://github.com/stardazed/sd-streams
  */
 
-const encEncoder = Symbol("encEncoder");
-const encTransform = Symbol("encTransform");
+const encEncoder = Symbol('encEncoder')
+const encTransform = Symbol('encTransform')
 
 class TextEncodeTransformer {
-	constructor(encoder) {
-		this.encoder_ = encoder;
-		this.partial_ = undefined;
-	}
+  constructor(encoder) {
+    this.encoder_ = encoder
+    this.partial_ = undefined
+  }
 
-	transform(chunk, controller) {
-		let stringChunk = String(chunk);
-		if (this.partial_ !== undefined) {
-			stringChunk = this.partial_ + stringChunk;
-			this.partial_ = undefined;
-		}
-		
-		const lastCharIndex = stringChunk.length - 1;
-		const lastCodeUnit = stringChunk.charCodeAt(lastCharIndex);
-		if (lastCodeUnit >= 0xD800 && lastCodeUnit < 0xDC00) {
-			this.partial_ = String.fromCharCode(lastCodeUnit);
-			stringChunk = stringChunk.substring(0, lastCharIndex);
-		}
+  transform(chunk, controller) {
+    let stringChunk = String(chunk)
+    if (this.partial_ !== undefined) {
+      stringChunk = this.partial_ + stringChunk
+      this.partial_ = undefined
+    }
 
-		const bytes = this.encoder_.encode(stringChunk);
-		if (bytes.length !== 0) {
-			controller.enqueue(bytes);
-		}
-	}
+    const lastCharIndex = stringChunk.length - 1
+    const lastCodeUnit = stringChunk.charCodeAt(lastCharIndex)
+    if (lastCodeUnit >= 0xd800 && lastCodeUnit < 0xdc00) {
+      this.partial_ = String.fromCharCode(lastCodeUnit)
+      stringChunk = stringChunk.substring(0, lastCharIndex)
+    }
 
-	flush(controller) {
-		if (this.partial_) {
-			controller.enqueue(this.encoder_.encode(this.partial_));
-			this.partial_ = undefined;
-		}
-	}
+    const bytes = this.encoder_.encode(stringChunk)
+    if (bytes.length !== 0) {
+      controller.enqueue(bytes)
+    }
+  }
+
+  flush(controller) {
+    if (this.partial_) {
+      controller.enqueue(this.encoder_.encode(this.partial_))
+      this.partial_ = undefined
+    }
+  }
 }
 
-export class TextEncoderStream  {
-	constructor() {
-		this[encEncoder] = new TextEncoder();
-		this[encTransform] = new TransformStream(new TextEncodeTransformer(this[encEncoder]));
-	}
+export class TextEncoderStream {
+  constructor() {
+    this[encEncoder] = new TextEncoder()
+    this[encTransform] = new TransformStream(
+      new TextEncodeTransformer(this[encEncoder])
+    )
+  }
 
-	get encoding() {
-		return this[encEncoder].encoding;
-	}
+  get encoding() {
+    return this[encEncoder].encoding
+  }
 
-	get readable() {
-		return this[encTransform].readable;
-	}
+  get readable() {
+    return this[encTransform].readable
+  }
 
-	get writable() {
-		return this[encTransform].writable;
-	}
+  get writable() {
+    return this[encTransform].writable
+  }
 }
 
 /**
@@ -152,56 +139,57 @@ export class TextEncoderStream  {
  * https://github.com/stardazed/sd-streams
  */
 
-const decDecoder = Symbol("decDecoder");
-const decTransform = Symbol("decTransform");
-
+const decDecoder = Symbol('decDecoder')
+const decTransform = Symbol('decTransform')
 
 class TextDecodeTransformer {
-	constructor(decoder) {
-		this.decoder_ = decoder;
-	}
+  constructor(decoder) {
+    this.decoder_ = decoder
+  }
 
-	transform(chunk, controller) {
-		if (! (chunk instanceof ArrayBuffer || ArrayBuffer.isView(chunk))) {
-			throw new TypeError("Input data must be a BufferSource");
-		}
-		const text = this.decoder_.decode(chunk, { stream: true });
-		if (text.length !== 0) {
-			controller.enqueue(text);
-		}
-	}
+  transform(chunk, controller) {
+    if (!(chunk instanceof ArrayBuffer || ArrayBuffer.isView(chunk))) {
+      throw new TypeError('Input data must be a BufferSource')
+    }
+    const text = this.decoder_.decode(chunk, {stream: true})
+    if (text.length !== 0) {
+      controller.enqueue(text)
+    }
+  }
 
-	flush(controller) {
-		const text = this.decoder_.decode();
-		if (text.length !== 0) {
-			controller.enqueue(text);
-		}
-	}
+  flush(controller) {
+    const text = this.decoder_.decode()
+    if (text.length !== 0) {
+      controller.enqueue(text)
+    }
+  }
 }
 
 export class TextDecoderStream {
-	constructor(label = 'utf-8', options = {}) {
-		this[decDecoder] = new TextDecoder(label, options);
-		this[decTransform] = new TransformStream(new TextDecodeTransformer(this[decDecoder]));
-	}
+  constructor(label = 'utf-8', options = {}) {
+    this[decDecoder] = new TextDecoder(label, options)
+    this[decTransform] = new TransformStream(
+      new TextDecodeTransformer(this[decDecoder])
+    )
+  }
 
-	get encoding() {
-		return this[decDecoder].encoding;
-	}
+  get encoding() {
+    return this[decDecoder].encoding
+  }
 
-	get fatal() {
-		return this[decDecoder].fatal;
-	}
+  get fatal() {
+    return this[decDecoder].fatal
+  }
 
-	get ignoreBOM() {
-		return this[decDecoder].ignoreBOM;
-	}
+  get ignoreBOM() {
+    return this[decDecoder].ignoreBOM
+  }
 
-	get readable() {
-		return this[decTransform].readable;
-	}
+  get readable() {
+    return this[decTransform].readable
+  }
 
-	get writable() {
-		return this[decTransform].writable;
-	}
+  get writable() {
+    return this[decTransform].writable
+  }
 }
